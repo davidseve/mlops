34acce81fa32a255c77fafac0d4fd466c1bdedd3
David Severiano <dseveria@redhat.com>
2024-06-27 16:13:47 +0200

    rhods

--- delete.sh
+++ delete.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+delete_subscription(){
+  currentCSV=$(oc get subscription $namespace -n $namespace -o yaml | grep currentCSV | sed 's/  currentCSV: //')
+  echo $currentCSV
+  oc delete subscription subscription $namespace -n $namespace
+  oc delete clusterserviceversion $currentCSV -n $namespace
+}
+
 set -x
 PATH=${PWD}/bin/:$PATH
 
@@ -13,6 +20,31 @@ currentCSV=$(oc get subscription openshift-pipelines-operator-rh -n openshift-op
 echo $currentCSV
 oc delete subscription openshift-pipelines-operator-rh -n openshift-operators
 oc delete clusterserviceversion $currentCSV -n openshift-operators
+
+#serverless deletion
+namespace=openshift-serverless
+subscription=serverless-operator
+
+#rhods deletion
+oc label configmap/delete-self-managed-odh api.openshift.com/addon-managed-odh-delete=true -n redhat-ods-operator
+sleep 1
+PROJECT_NAME=redhat-ods-applications
+while oc get project $PROJECT_NAME &> /dev/null; do
+  echo "The $PROJECT_NAME project still exists"
+  sleep 1
+done
+echo "The $PROJECT_NAME project no longer exists"
+oc delete namespace redhat-ods-operator
+oc delete namespace redhat-ods-applications
+oc delete namespace redhat-ods-monitoring
+oc delete namespace redhat-ods-operator
+oc delete namespace rhods-notebooks
+
+currentCSV=$(oc get subscription rhods-operator -n redhat-ods-operator -o yaml | grep currentCSV | sed 's/  currentCSV: //')
+echo $currentCSV
+oc delete subscription subscription rhods-operator -n redhat-ods-operator
+oc delete clusterserviceversion $currentCSV -n redhat-ods-operator
+
 
 #delete  openshift-gitops operator resources
 if [[ ${1:-1} = "1" ]]; then
