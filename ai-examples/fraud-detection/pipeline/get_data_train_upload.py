d3549b03626003a1148c593f2d646459a44281db
David Severiano <dseveria@redhat.com>
2024-07-12 18:17:26 +0200

    add_pipelines

--- ai-examples/fraud-detection/pipeline/get_data_train_upload.py
+++ ai-examples/fraud-detection/pipeline/get_data_train_upload.py
@@ -163,8 +163,101 @@ def pipeline():
             'AWS_S3_ENDPOINT': 'AWS_S3_ENDPOINT',
         })
 
+# Get the service account token or return None
+def get_token():
+    try:
+        with open("/var/run/secrets/kubernetes.io/serviceaccount/token", "r") as f:
+            return f.read().strip()
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+# Get the route host for the specified route name in the specified namespace
+def get_route_host(route_name: str):
+    # Load in-cluster Kubernetes configuration but if it fails, load local configuration
+    try:
+        config.load_incluster_config()
+    except config.config_exception.ConfigException:
+        config.load_kube_config()
+
+    # Get the current namespace
+    with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r") as f:
+        namespace = f.read().strip()
+
+    # Create Kubernetes API client
+    api_instance = client.CustomObjectsApi()
+
+    try:
+        # Retrieve the route object
+        route = api_instance.get_namespaced_custom_object(
+            group="route.openshift.io",
+            version="v1",
+            namespace=namespace,
+            plural="routes",
+            name=route_name
+        )
+
+        # Extract spec.host field
+        route_host = route['spec']['host']
+        return route_host
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
 if __name__ == '__main__':
     compiler.Compiler().compile(
         pipeline_func=pipeline,
         package_path=__file__.replace('.py', '.yaml')
     )
+
+    # Take token and kfp_endpoint as optional command-line arguments
+    token = sys.argv[1] if len(sys.argv) > 1 else None
+    kfp_endpoint = sys.argv[2] if len(sys.argv) > 2 else None
+
+    if not token:
+        print("Token endpoint not provided finding it automatically.")
+        token = get_token()
+
+    if not kfp_endpoint:
+        print("KFP endpoint not provided finding it automatically.")
+        kfp_endpoint = get_route_host(route_name="ds-pipeline-dspa")
+
+    # Pipeline name
+    pipeline_name = os.path.basename(__file__).replace('.py', '')
+
+    # If both kfp_endpoint and token are provided, upload the pipeline
+    if kfp_endpoint and token:
+        client = kfp.Client(host=kfp_endpoint, existing_token=token)
+
+        # If endpoint doesn't have a protocol (http or https), add https
+        if not kfp_endpoint.startswith("http"):
+            kfp_endpoint = f"https://{kfp_endpoint}"
+
+        try:
+            # Get the pipeline by name
+            print(f"Pipeline name: {pipeline_name}")
+            existing_pipeline = get_pipeline_by_name(client, pipeline_name)
+            if existing_pipeline:
+                print(f"Pipeline {existing_pipeline.pipeline_id} already exists. Uploading a new version.")
+                # Upload a new version of the pipeline with a version name equal to the pipeline package path plus a timestamp
+                pipeline_version_name=f"{pipeline_name}-{int(time.time())}"
+                client.upload_pipeline_version(
+                    pipeline_package_path=pipeline_package_path,
+                    pipeline_id=existing_pipeline.pipeline_id,
+                    pipeline_version_name=pipeline_version_name
+                )
+                print(f"Pipeline version uploaded successfully to {kfp_endpoint}")
+            else:
+                print(f"Pipeline {pipeline_name} does not exist. Uploading a new pipeline.")
+                print(f"Pipeline package path: {pipeline_package_path}")
+                # Upload the compiled pipeline
+                client.upload_pipeline(
+                    pipeline_package_path=pipeline_package_path,
+                    pipeline_name=pipeline_name
+                )
+                print(f"Pipeline uploaded successfully to {kfp_endpoint}")
+        except Exception as e:
+            print(f"Failed to upload the pipeline: {e}")
+    else:
+        print("KFP endpoint or token not provided. Skipping pipeline upload.")
