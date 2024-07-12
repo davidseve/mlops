ccae98de232edb9da2918ce2664ece0ac4c59729
David Severiano <dseveria@redhat.com>
2024-07-12 18:54:39 +0200

    add_pipelines

--- ai-examples/fraud-detection/pipeline/get_data_train_upload.py
+++ ai-examples/fraud-detection/pipeline/get_data_train_upload.py
@@ -236,11 +236,11 @@ if __name__ == '__main__':
 
     # If both kfp_endpoint and token are provided, upload the pipeline
     if kfp_endpoint and token:
-        client = kfp.Client(host=kfp_endpoint, existing_token=token, ssl_ca_cert=None,)
-
         # If endpoint doesn't have a protocol (http or https), add https
         if not kfp_endpoint.startswith("http"):
             kfp_endpoint = f"https://{kfp_endpoint}"
+            
+        client = kfp.Client(host=kfp_endpoint, existing_token=token, ssl_ca_cert=None,)
 
         try:
             # Get the pipeline by name
