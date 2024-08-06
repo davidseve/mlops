e70c76d57eb6fc4c12e5b4568fb1550d75663cd9
David Severiano <dseveria@redhat.com>
2024-08-06 14:41:51 +0200

    create train model component

--- ai-examples/fraud-detection/testing/test-fraud.sh
+++ ai-examples/fraud-detection/testing/test-fraud.sh
@@ -6,23 +6,10 @@ appname=ai-fraud-example
 workbench=workbench-one
 namespace=fraud
 
-oc apply -f $appfile
 
-# wait until $appname is available
-status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.resources.sync.status }')
-while [[ "${status}" != "Healthy" ]]; do
-  sleep 5;
-  status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.health.status }')
-done
-
-
-sleep 30
-
-tkn pipeline start pipeline-upload-pipeline-one --workspace name=workspace-source,claimName=pipeline-upload-pipeline-one-source-pvc -n $namespace --showlog --use-param-defaults
-
-sleep 10
 
 # Run pipeline
+cd kfp
 oc apply -f kfp-run-pipelines-task.yaml
 oc apply -f kfp-run-pipelines-pipeline.yaml
 tkn pipeline start pipeline-run-pipelines -n $namespace --showlog
