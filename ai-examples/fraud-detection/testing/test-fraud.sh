daf9fbf1b67dc1e79ff5ea6084495c30dccb6ed5
David Severiano <dseveria@redhat.com>
2024-08-13 17:49:01 +0200

    add inference to chart

--- ai-examples/fraud-detection/testing/test-fraud.sh
+++ ai-examples/fraud-detection/testing/test-fraud.sh
@@ -10,8 +10,16 @@ oc apply -f $appfile
 # wait until $appname is available
 status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.resources.sync.status }')
 while [[ "${status}" != "Healthy" ]]; do
-  sleep 5;
+  sleep 20;
+  echo "Wait for app Healthy 20s"
   status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.health.status }')
 done
 
-
+sleep 30
+# wait until $appname is available
+status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.resources.sync.status }')
+while [[ "${status}" != "Healthy" ]]; do
+  sleep 10;
+  echo "Wait for app Healthy 10s"
+  status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.health.status }')
+done
