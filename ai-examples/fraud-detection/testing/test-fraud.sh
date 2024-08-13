80893424bb86f49864df3bc1f3a15f9b14bffee6
David Severiano <dseveria@redhat.com>
2024-08-13 10:13:56 +0200

    add inference to chart

--- ai-examples/fraud-detection/testing/test-fraud.sh
+++ ai-examples/fraud-detection/testing/test-fraud.sh
@@ -23,9 +23,6 @@ tkn pipeline start pipeline-upload-pipeline-one --workspace name=workspace-sourc
 sleep 10
 
 # Run pipeline
-cd kfp
-oc apply -f kfp-run-pipelines-task.yaml
-oc apply -f kfp-run-pipelines-pipeline.yaml
 tkn pipeline start pipeline-run-pipelines -n $namespace --showlog
 # Validate AI pipelines is working
 # ../../testing/ns-workflows-running.sh $namespace
