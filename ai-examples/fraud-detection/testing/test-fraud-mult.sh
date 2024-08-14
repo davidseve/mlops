d1a3fa559972b476dae9aa32b3932cdf5f6afcda
David Severiano <dseveria@redhat.com>
2024-08-14 16:42:22 +0200

    fix multi model

--- ai-examples/fraud-detection/testing/test-fraud-mult.sh
+++ ai-examples/fraud-detection/testing/test-fraud-mult.sh
@@ -3,7 +3,6 @@
 
 appfile=../gitops/mult/app-ai-fraud-mult.yaml 
 appname=ai-fraud-example-mult
-namespace=fraud-mult
 
 oc apply -f $appfile
 
@@ -23,3 +22,49 @@ while [[ "${status}" != "Healthy" ]]; do
   echo "Wait for app Healthy 10s"
   status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.health.status }')
 done
+ 
+# The URL for the curl command
+url="https://fraud-multinference-fraud-mult.apps.dseveria.sandbox664.opentlc.com/v2/models/fraud-multinference/versions/1/infer" 
+data='{
+          "id" : "42",
+          "inputs": [
+                    {
+                        "name": "dense_input",
+                        "shape": [1, 5],
+                        "datatype": "FP32",
+                        "data": [0.3111400080477545, 1.9459399775518593, 1.0, 0.0, 0.0]
+                    }
+                ]
+        }'
+
+
+# Expected response body
+expected_response='{"name":"fraud-multinference","versions":["1"],"platform":"OpenVINO","inputs":[{"name":"dense_input","datatype":"FP32","shape":[-1,5]}],"outputs":[{"name":"dense_3","datatype":"FP32","shape":[-1,1]}]}'
+
+# Retry interval in seconds
+retry_interval=5
+
+while true; do
+    # Perform the curl command and capture the response and HTTP status code
+    response=$(curl -k -s -w "%{http_code}" -o response_body.txt -X POST "$url" -H "Content-Type: application/json" -d "$data" )
+    http_code="${response: -3}"
+    body=$(<response_body.txt)
+    
+    # Check if the HTTP status code is 200 and the response body matches the expected response
+    if [[ "$http_code" == "200" ]]; then
+        echo "Success: Received HTTP 200 and expected response."
+        echo $expected_response
+        break
+    else
+        echo "Failed: HTTP Code $http_code or unexpected response. Retrying in $retry_interval seconds..."
+        sleep "$retry_interval"
+    fi
+done
+
+curl -k https://fraud-multinference-predictor-fraud-mult.apps.dseveria.sandbox664.opentlc.com/v2/models/fraud-multinference
+
+
+# Clean up
+rm response_body.txt
+
+
