728313f4fa70974866e82f385e441e0915a5db28
David Severiano <dseveria@redhat.com>
2024-08-14 10:06:08 +0200

    fix inference

--- ai-examples/fraud-detection/testing/test-fraud.sh
+++ ai-examples/fraud-detection/testing/test-fraud.sh
@@ -23,3 +23,43 @@ while [[ "${status}" != "Healthy" ]]; do
   echo "Wait for app Healthy 10s"
   status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.health.status }')
 done
+
+# The URL for the curl command
+url="https://fraud-inference-fraud.apps.dseveria.sandbox1723.opentlc.com/v2/models/fraud-inference/versions/1/infer" 
+data="'{
+          "id" : "42",
+          "inputs": [
+                    {
+                        "name": "dense_input",
+                        "shape": [1, 5],
+                        "datatype": "FP32",
+                        "data": [0.3111400080477545, 1.9459399775518593, 1.0, 0.0, 0.0]
+                    }
+                ]
+        }'"
+
+
+# Expected response body
+expected_response='{"name":"fraud-inference","versions":["1"],"platform":"OpenVINO","inputs":[{"name":"dense_input","datatype":"FP32","shape":[-1,5]}],"outputs":[{"name":"dense_3","datatype":"FP32","shape":[-1,1]}]}'
+
+# Retry interval in seconds
+retry_interval=5
+
+while true; do
+    # Perform the curl command and capture the response and HTTP status code
+    response=$(curl -k -X POST -s -w "%{http_code}" -o response_body.txt "$url" -d "$data")
+    http_code="${response: -3}"
+    body=$(<response_body.txt)
+    
+    # Check if the HTTP status code is 200 and the response body matches the expected response
+    if [[ "$http_code" == "200" && "$body" == "$expected_response" ]]; then
+        echo "Success: Received HTTP 200 and expected response."
+        break
+    else
+        echo "Failed: HTTP Code $http_code or unexpected response. Retrying in $retry_interval seconds..."
+        sleep "$retry_interval"
+    fi
+done
+
+# Clean up
+rm response_body.txt
