28db56d16a62e7764e0029f30a091f8d43d23183
David Severiano <dseveria@dseveria-thinkpadp1gen7.rmtes.csb>
2025-03-20 12:47:11 +0100

    improve tests

--- ai-examples/fraud-detection/testing/test-fraud.sh
+++ ai-examples/fraud-detection/testing/test-fraud.sh
@@ -5,41 +5,26 @@ testAPI(){
     if [ "$#" -gt 0 ]; then
         # The URL for the curl command
         url="https://fraudinference-fraud.apps.$1/v2/models/fraudinference/versions/1/infer" 
-        data='{
-                "id" : "42",
-                "inputs": [
-                            {
-                                "name": "dense_input",
-                                "shape": [1, 5],
-                                "datatype": "FP32",
-                                "data": [0.3111400080477545, 1.9459399775518593, 1.0, 0.0, 0.0]
-                            }
-                        ]
-                }'
-
-
-        # Expected response body
-        expected_response='{"name":"fraudinference","versions":["1"],"platform":"OpenVINO","inputs":[{"name":"dense_input","datatype":"FP32","shape":[-1,5]}],"outputs":[{"name":"dense_3","datatype":"FP32","shape":[-1,1]}]}'
 
         # Retry interval in seconds
         retry_interval=5
 
         while true; do
             # Perform the curl command and capture the response and HTTP status code
-            response=$(curl -k -s -w "%{http_code}" -o response_body.txt -X POST "$url" -H "Content-Type: application/json" -d "$data" )
+            response=$(curl -k -s -w "%{http_code}" -o response_body.txt -X POST "$url" -H "Content-Type: application/json" -d "$2" )
             http_code="${response: -3}"
-            body=$(<response_body.txt)
             
             # Check if the HTTP status code is 200 and the response body matches the expected response
             if [[ "$http_code" == "200" ]]; then
                 echo "Success: Received HTTP 200 and expected response."
-                echo $expected_response
+                cat response_body.txt
                 break
             else
                 echo "Failed: HTTP Code $http_code or unexpected response. Retrying in $retry_interval seconds..."
+                cat response_body.txt
                 sleep "$retry_interval"
             fi
-            echo $body
+            
         done
 
         # Clean up
@@ -84,14 +69,26 @@ while [[ "${status}" != "Healthy" ]]; do
   status=$(oc get application.argoproj.io $appname -n openshift-gitops -o jsonpath='{ .status.health.status }')
 done
 
-testAPI $1
+# Fraud
+data='{
+                "id" : "42",
+                "inputs": [
+                            {
+                                "name": "dense_input",
+                                "shape": [1, 5],
+                                "datatype": "FP32",
+                                "data": [0.3111400080477545, 1.9459399775518593, 1.0, 0.0, 0.0]
+                            }
+                        ]
+                }'
+testAPI $1 "$data"
 
 # Define variables
 NAMESPACE="fraud"
 PIPELINE_RUN_NAME="pipeline-run-pipeline-one"
 MODEL_VERSION="2"
 
-tkn pipeline start $PIPELINE_RUN_NAME --param MODEL_VERSION=$MODEL_VERSION --param CARDTRANSDATA=https://raw.githubusercontent.com/davidseve/mlops/main/ai-examples/fraud-detection/data/card_transdata.csv-n $NAMESPACE --showlog
+tkn pipeline start $PIPELINE_RUN_NAME --param MODEL_VERSION=$MODEL_VERSION --param CARDTRANSDATA=https://raw.githubusercontent.com/davidseve/mlops/main/ai-examples/fraud-detection/data/card_transdata.csv -n $NAMESPACE --showlog
 
 sed -i 's/version: 1/version: 2/' ai-examples/fraud-detection/gitops/values-fraud.yaml
 
@@ -99,9 +96,35 @@ git add ai-examples/fraud-detection/gitops/values-fraud.yaml
 git commit -m "Change model version to v2"
 git push origin $appname
 
-argocd sync app $appname
+argocd app sync $appname
 
-sleep 1
+sleep 30
 
-testAPI $1
+# Fraud
+data='{
+                "id" : "42",
+                "inputs": [
+                            {
+                                "name": "dense_input",
+                                "shape": [1, 5],
+                                "datatype": "FP32",
+                                "data": [0.3111400080477545, 1.9459399775518593, 1.0, 0.0, 0.0]
+                            }
+                        ]
+                }'
+testAPI $1 "$data"
 
+# Not fraud
+data='{
+                "id" : "42",
+                "inputs": [
+                            {
+                                "name": "dense_input",
+                                "shape": [1, 5],
+                                "datatype": "FP32",
+                                "data": [0.0, 0.0, 1.0, 1.0, 0.0]
+                            }
+                        ]
+                }'
+testAPI $1 "$data"
+         
